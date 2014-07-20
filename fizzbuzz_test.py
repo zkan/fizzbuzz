130f8f1f2909cb9a89058fccc19c2eca198d9c35
prontokan <kan@prontomarketing.com>
2014-07-20 17:17:15 +0700

    input number divisible by 3 then return fizz :bear:

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,17 +2,29 @@ import unittest
 
 
 class FizzBuzzTest(unittest.TestCase):
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
     def test_input_number_should_return_same_number(self):
-        fizzbuzz = FizzBuzz()
-        result = fizzbuzz.take(1)
+        result = self.fizzbuzz.take(1)
         self.assertEqual(result, 1)
 
-        result = fizzbuzz.take(2)
+        result = self.fizzbuzz.take(2)
         self.assertEqual(result, 2)
+
+    def test_input_number_divisible_by_3_should_return_fizz(self):
+        result = self.fizzbuzz.take(3)
+        self.assertEqual(result, 'fizz')
+
+        result = self.fizzbuzz.take(6)
+        self.assertEqual(result, 'fizz')
 
 
 class FizzBuzz:
     def take(self, number):
+        if number % 3 == 0:
+            return 'fizz'
+
         return number
 
 
