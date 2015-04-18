cde7f86584dfd8f0d781e772748a7622ea83d9d1
zkan <kan@prontomarketing.com>
2015-04-18 19:51:57 +0700

    separate methods for fizz and buzz

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,10 +1,18 @@
 class FizzBuzz
+    def fizz?(number)
+        number % 3 == 0
+    end
+
+    def buzz?(number)
+        number % 5 == 0
+    end
+
     def take(number)
-        if number % 3 == 0 and number % 5 == 0
+        if fizz?(number) and buzz?(number)
             "fizzbuzz"
-        elsif number % 3 == 0
+        elsif fizz?(number)
             "fizz"
-        elsif number % 5 == 0
+        elsif buzz?(number)
             "buzz"
         else
             number
