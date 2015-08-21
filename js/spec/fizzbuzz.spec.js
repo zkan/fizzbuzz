4baef3ee1d36fdeff7564966afff5b666c7e9605
zkan <kan@prontomarketing.com>
2015-08-21 21:41:44 +0700

    group the test scenarios

--- js/spec/fizzbuzz.spec.js
+++ js/spec/fizzbuzz.spec.js
@@ -5,43 +5,51 @@ describe("FizzBuzz", function() {
         app = require("../app/fizzbuzz");
     });
 
-    it("should return fizz if input is 3", function() {
-        result = app.fizzbuzz(3);
-        expect(result).toBe("fizz");
+    describe("fizz is returned if input is divisible by 3", function() {
+        it("should return fizz if input is 3", function() {
+            result = app.fizzbuzz(3);
+            expect(result).toBe("fizz");
+        });
+
+        it("should return fizz if input is 6", function() {
+            result = app.fizzbuzz(6);
+            expect(result).toBe("fizz");
+        });
     });
 
-    it("should return fizz if input is 6", function() {
-        result = app.fizzbuzz(6);
-        expect(result).toBe("fizz");
+    describe("buzz is returned if input is divisible by 5", function() {
+        it("should return buzz if input is 5", function() {
+            result = app.fizzbuzz(5);
+            expect(result).toBe("buzz");
+        });
+
+        it("should return buzz if input is 10", function() {
+            result = app.fizzbuzz(10);
+            expect(result).toBe("buzz");
+        });
     });
 
-    it("should return buzz if input is 5", function() {
-        result = app.fizzbuzz(5);
-        expect(result).toBe("buzz");
+    describe("fizzbuzz is returned if input is divisible by 3 and 5", function() {
+        it("should return fizzbuzz if input is 15", function() {
+            result = app.fizzbuzz(15);
+            expect(result).toBe("fizzbuzz");
+        });
+
+        it("should return fizzbuzz if input is 30", function() {
+            result = app.fizzbuzz(30);
+            expect(result).toBe("fizzbuzz");
+        });
     });
 
-    it("should return buzz if input is 10", function() {
-        result = app.fizzbuzz(10);
-        expect(result).toBe("buzz");
-    });
+    describe("input itself is returned if input is not divisible by 3 and 5", function() {
+        it("should return 2 if input is 2", function() {
+            result = app.fizzbuzz(2);
+            expect(result).toBe(2);
+        });
 
-    it("should return fizzbuzz if input is 15", function() {
-        result = app.fizzbuzz(15);
-        expect(result).toBe("fizzbuzz");
-    });
-
-    it("should return fizzbuzz if input is 30", function() {
-        result = app.fizzbuzz(30);
-        expect(result).toBe("fizzbuzz");
-    });
-
-    it("should return 2 if input is 2", function() {
-        result = app.fizzbuzz(2);
-        expect(result).toBe(2);
-    });
-
-    it("should return 7 if input is 7", function() {
-        result = app.fizzbuzz(7);
-        expect(result).toBe(7);
+        it("should return 7 if input is 7", function() {
+            result = app.fizzbuzz(7);
+            expect(result).toBe(7);
+        });
     });
 });
