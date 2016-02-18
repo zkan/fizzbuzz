bd97c302b920bb46bca7f87fbb45c9eb7a801720
zkan <kan@prontomarketing.com>
2016-02-18 08:58:14 +0700

    Get Buzz if input is 5

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -4,6 +4,8 @@ class FizzBuzz {
     function take( $number ) {
         if ( 0 == $number % 3 ) {
             return "Fizz";
+        } elseif ( 5 == $number ) {
+            return "Buzz";
         }
     }
 }
--- php/tests/unit/FizzBuzzTest.php
+++ php/tests/unit/FizzBuzzTest.php
@@ -8,6 +8,7 @@ class FizzBuzzTest extends \Codeception\TestCase\Test {
     protected function _before() {
         $this->fizzbuzz = new FizzBuzz();
     }
+
     public function testInputThreeShouldReturnFizz() {
         $result = $this->fizzbuzz->take( 3 );
         $this->assertEquals( "Fizz", $result );
@@ -16,5 +17,10 @@ class FizzBuzzTest extends \Codeception\TestCase\Test {
     public function testInputSixShouldReturnFizz() {
         $result = $this->fizzbuzz->take( 6 );
         $this->assertEquals( "Fizz", $result );
+    }
+
+    public function testInputFiveShouldReturnBuzz() {
+        $result = $this->fizzbuzz->take( 5 );
+        $this->assertEquals( "Buzz", $result );
     }
 }
