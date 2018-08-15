6ec9d69161a4e3178ed09fd5bb65f329809f4a39
zkan <kan@prontomarketing.com>
2018-08-15 21:22:39 +0700

    Complete FizzBuzz with pattern matching :bear:

--- scala/src/main/scala/FizzBuzz.scala
+++ scala/src/main/scala/FizzBuzz.scala
@@ -1,11 +1,12 @@
 package fizzbuzz
 
 object FizzBuzz {
-  def say(number: Int): Either[String, Unit] = {
-    if(number % 3 == 0) {
-      Left("Fizz")
-    } else {
-      Right(None)
+  def say(number: Int) = {
+    (number % 3, number % 5) match {
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _ => number
     }
   }
 }
--- scala/src/test/scala/FizzBuzz.scala
+++ scala/src/test/scala/FizzBuzz.scala
@@ -4,10 +4,34 @@ import org.scalatest.{FunSuite, Matchers}
 
 class FizzBuzzSuite extends FunSuite with Matchers {
   test("Input 3 should get Fizz") {
-    FizzBuzz.say(3) shouldBe Left("Fizz")
+    FizzBuzz.say(3) shouldBe "Fizz"
   }
 
   test("Input 6 should get Fizz") {
-    FizzBuzz.say(6) shouldBe Left("Fizz")
+    FizzBuzz.say(6) shouldBe "Fizz"
+  }
+
+  test("Input 5 should get Buzz") {
+    FizzBuzz.say(5) shouldBe "Buzz"
+  }
+
+  test("Input 10 should get Buzz") {
+    FizzBuzz.say(10) shouldBe "Buzz"
+  }
+
+  test("Input 15 should get FizzBuzz") {
+    FizzBuzz.say(15) shouldBe "FizzBuzz"
+  }
+
+  test("Input 30 should get FizzBuzz") {
+    FizzBuzz.say(30) shouldBe "FizzBuzz"
+  }
+
+  test("Input 1 should get 1") {
+    FizzBuzz.say(1) shouldBe 1
+  }
+
+  test("Input 4 should get 4") {
+    FizzBuzz.say(4) shouldBe 4
   }
 }
