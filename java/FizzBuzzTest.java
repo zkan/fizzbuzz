4012a4340d975d5612fb1f4d09a94f2ce9bf8321
zkan <kan@prontomarketing.com>
2020-03-12 09:21:04 +0700

    :arrow_up: Switch to JUnit 5 :bear:

--- java/FizzBuzzTest.java
+++ java/FizzBuzzTest.java
@@ -1,5 +1,5 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     FizzBuzz fizzBuzz = new FizzBuzz();
--- java/test.sh
+++ java/test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-javac -cp .:junit-4.13.jar FizzBuzzTest.java
-java -cp .:junit-4.13.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore FizzBuzzTest
+# JUnit 4
+#javac -cp .:junit-4.13.jar FizzBuzzTest.java
+#java -cp .:junit-4.13.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore FizzBuzzTest
+
+# JUnit 5
+javac -cp .:junit-platform-console-standalone-1.6.0.jar FizzBuzzTest.java
+java -jar junit-platform-console-standalone-1.6.0.jar --class-path . --scan-classpath
