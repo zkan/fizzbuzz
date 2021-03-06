e1501c1aa85b5d5761bd75c47834b76f07aaf83a
zkan <zkan.cs@gmail.com>
2021-03-06 10:12:21 +0700

    Get number divisible by 5 then return Buzz :bear:

--- java/fizzbuzz/src/main/java/FizzBuzz.java
+++ java/fizzbuzz/src/main/java/FizzBuzz.java
@@ -2,6 +2,8 @@ public class FizzBuzz {
     public String get(int number) {
         if (number % 3 == 0) {
             return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(number);
     }
--- java/fizzbuzz/src/test/java/FizzBuzzTest.java
+++ java/fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -1,34 +1,51 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+    FizzBuzz fizzbuzz;
+
+    @BeforeEach
+    void setUp() {
+        this.fizzbuzz = new FizzBuzz();
+    }
+
     @Test
     void testGetOneShouldReturnOne() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String actual = fizzbuzz.get(1);
+        String actual = this.fizzbuzz.get(1);
         Assertions.assertEquals("1", actual);
     }
 
     @Test
     void testGetTwoShouldReturnTwo() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String actual = fizzbuzz.get(2);
+        String actual = this.fizzbuzz.get(2);
         Assertions.assertEquals("2", actual);
     }
 
     @Test
     void testGetThreeShouldReturnFizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.get(3);
+        String actual = this.fizzbuzz.get(3);
         Assertions.assertEquals("Fizz", actual);
     }
 
     @Test
-    void testGetSizShouldReturnFizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.get(6);
+    void testGetSixShouldReturnFizz() {
+        String actual = this.fizzbuzz.get(6);
         Assertions.assertEquals("Fizz", actual);
     }
+
+    @Test
+    void testGetFiveShouldReturnBuzz() {
+        String actual = this.fizzbuzz.get(5);
+        Assertions.assertEquals("Buzz", actual);
+    }
+
+    @Test
+    void testGetTenShouldReturnBuzz() {
+        String actual = this.fizzbuzz.get(10);
+        Assertions.assertEquals("Buzz", actual);
+    }
+
 }
