0cfc1bb92c7f72ce46c69dd4ef055379484dfcb1
zkan <zkan.cs@gmail.com>
2021-03-06 10:06:02 +0700

    Get 2 then return 2 :bear:

--- java/fizzbuzz/src/main/java/FizzBuzz.java
+++ java/fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,8 +1,5 @@
 public class FizzBuzz {
     public String get(int number) {
-        if (number == 1) {
-            return "1";
-        }
-        return null;
+        return String.valueOf(number);
     }
 }
--- java/fizzbuzz/src/test/java/FizzBuzzTest.java
+++ java/fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -10,4 +10,11 @@ class FizzBuzzTest {
         String actual = fizzbuzz.get(1);
         Assertions.assertEquals("1", actual);
     }
+
+    @Test
+    void testGetTwoShouldReturnTwo() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String actual = fizzbuzz.get(2);
+        Assertions.assertEquals("2", actual);
+    }
 }
