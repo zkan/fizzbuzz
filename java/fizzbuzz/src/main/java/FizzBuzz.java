cadfbdd658b0c75fb9ecbb0105fb6ebf2302a78d
zkan <zkan.cs@gmail.com>
2021-03-06 10:15:46 +0700

    Adjust coding style :bear:

--- java/fizzbuzz/src/main/java/FizzBuzz.java
+++ java/fizzbuzz/src/main/java/FizzBuzz.java
@@ -2,8 +2,7 @@ public class FizzBuzz {
     public String get(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
-        }
-        else if (number % 3 == 0) {
+        } else if (number % 3 == 0) {
             return "Fizz";
         } else if (number % 5 == 0) {
             return "Buzz";
